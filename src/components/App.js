148d9da61e115a8fce8859a0a6eae17c0a816e18
Pravaritti <pravaritti.1995@gmail.com>
2022-07-14 17:45:06 +0530

    update state with fetched data

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,21 @@ import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
+  state = { videos: [] };
+  onTermSubmit = async (term) => {
     console.log(term);
-    youtube.get("/search", {
+
+    //fetch list of videos from API
+    // making a request is an asynchronous operation so we interact with it eitehr using promises or async wait syntax
+    const response = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
+
+    console.log(response); //entire response object that has ton of information about the request that was just made.
+    //response -> data -> items = list of videos we are interested in
+    this.setState({ videos: response.data.items });
   };
 
   render() {
@@ -18,6 +26,7 @@ class App extends React.Component {
         {/* putting classNAme in div gives it a container like boundary */}
         <SearchBar onFormSubmit={this.onTermSubmit} />
         {/*name of the prop could be anything of our choice -> it has callback Function. */}
+        I have {this.state.videos.length} videos.
       </div>
     );
   }
